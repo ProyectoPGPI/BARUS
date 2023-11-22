b3e90b87750232a36caa81168178596562b61108
ferbarbar1 <ferbarbar1@alum.us.es>
2023-11-22 12:46:54 +0100

    add(initial):RF-020
    Added user registration

--- proyectopgpi/settings.py
+++ proyectopgpi/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'pgpi',
+    'django.contrib.staticfiles', 
+    'authentication',   
 ]
 
 MIDDLEWARE = [
@@ -74,12 +74,6 @@ WSGI_APPLICATION = 'proyectopgpi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-'''DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
