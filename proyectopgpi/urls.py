f5ee5ad4b6d8cd1a2ddf14c687a1a121793de994
ferbarbar1 <ferbarbar1@alum.us.es>
2023-11-22 13:27:50 +0100

    add(initial):RF-020
    Added user registration

--- proyectopgpi/urls.py
+++ proyectopgpi/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from authentication import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('pgpi.urls'))
+    path('',include('pgpi.urls')),
+    path('signup/', views.signup,name='signup'),
 ]
