30d82c4e4e3016864858a2a8839d3cdc478327e6
gonsanmar2 <gonsanmar2@alum.us.es>
2023-11-28 18:34:09 +0100

    carrito para usuarios no logueados hecho y numero de productos en carrito

--- carrito/models.py
+++ carrito/models.py
@@ -5,15 +5,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Carrito(models.Model):
+    cliente = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     productos = models.ManyToManyField(Producto, through='ItemCarrito')
-    cliente = models.ForeignKey(User, on_delete=models.CASCADE)
     total = models.DecimalField(default=0, max_digits=10, decimal_places=2)
 
-    def calcularTotal(self):
+    def calcular_total(self):
         total = sum(item.producto.precio * item.cantidad for item in self.itemcarrito_set.all())
         self.total = total
         self.save()
-        
+    
+    def obtener_cantidad_total(self):
+        return sum(item.cantidad for item in self.itemcarrito_set.all())
+
 class ItemCarrito(models.Model):
     carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
