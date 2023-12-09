bd20eab0d81403d0f5f2d4a02113929fb723cf81
ferbarbar1 <ferbarbar1@alum.us.es>
2023-12-09 12:25:25 +0100

    Añadido formulario de reclamaciones para clientes

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Reclamacion(models.Model):
+    ESTADOS = (
+        ('pendiente', 'Pendiente'),
+        ('revisada', 'Revisada'),
+        ('cancelada', 'Cancelada'),
+    )
+
+    titulo = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    estado = models.CharField(max_length=20, choices=ESTADOS, default='pendiente')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
